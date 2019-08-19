f3f62562a885b309d6840f43fbe50e838b1ccfc7
Kevin Tou <toukevin@gmail.com>
2019-08-19 16:08:38 -0700

    Created tests for components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -10,4 +10,72 @@ describe('<Controls />', () => {
 
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it('toggleClosed function upon event click', () => {
+    const toggledClosed = jest.fn();
+    const controls = render(<Controls toggleClosed={toggledClosed} />);
+    const gateButton = controls.getByText(/close gate/i);
+
+    fireEvent.click(gateButton);
+
+    expect(toggledClosed).toHaveBeenCalled();
+  });
+
+  it('should display "Lock Gate" and "Close Gate" when props are false', () => {
+    const props = {
+      locked: false,
+      closed: false,
+    };
+
+    const controls = render(
+      <Controls locked={props.locked} closed={props.closed} />,
+    );
+
+    expect(controls.getByText(/lock gate/i).textContent).toBe('Lock Gate');
+    expect(controls.getByText(/close gate/i).textContent).toBe('Close Gate');
+  });
+
+  it('should display "Unlock Gate" and "Open Gate" when props are true', () => {
+    const props = {
+      locked: true,
+      closed: true,
+    };
+
+    const controls = render(
+      <Controls locked={props.locked} closed={props.closed} />,
+    );
+
+    expect(controls.getByText(/unlock gate/i).textContent).toBe('Unlock Gate');
+    expect(controls.getByText(/open gate/i).textContent).toBe('Open Gate');
+  });
+
+  it('the closed toggle button is disabled if the gate is locked', () => {
+    const props = {
+      locked: true,
+      closed: true,
+    };
+
+    const controls = render(
+      <Controls locked={props.locked} closed={props.closed} />,
+    );
+
+    expect(
+      controls.getByText(/open gate/i).hasAttribute('disabled'),
+    ).toBeTruthy();
+  });
+
+  it('the locked toggle button is disabled if the gate is open', () => {
+    const props = {
+      locked: false,
+      closed: false,
+    };
+
+    const controls = render(
+      <Controls locked={props.locked} closed={props.closed} />,
+    );
+
+    expect(
+      controls.getByText(/lock gate/i).hasAttribute('disabled'),
+    ).toBeTruthy();
+  });
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -10,4 +10,33 @@ describe('<Dashboard />', () => {
 
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  // Same test as defaulting to 'unlocked' and 'open'
+  it('it shows the controls and display', () => {
+    const dashboard = render(<Dashboard />);
+
+    const lockStatus = dashboard.getByText(/unlocked/i);
+    const gateStatus = dashboard.getByText(/open/i);
+    const lockButton = dashboard.getByText(/lock gate/i);
+    const gateButton = dashboard.getByText(/close gate/i);
+  });
+
+  it('cannot be closed or opened if it is locked', () => {
+    const dashboard = render(<Dashboard />);
+
+    expect(dashboard.queryByText(/Open Gate/i)).toBeFalsy();
+
+    fireEvent.click(dashboard.getByText(/Close Gate/i));
+    fireEvent.click(dashboard.getByText(/Lock Gate/i));
+
+    expect(dashboard.queryByText(/Open Gate/i)).toBeTruthy();
+
+    expect(
+      dashboard.queryByText(/Open Gate/i).hasAttribute('disabled'),
+    ).toBeTruthy();
+
+    expect(
+      dashboard.queryByText(/Unlock Gate/i).hasAttribute('disabled'),
+    ).toBeFalsy();
+  });
 });
